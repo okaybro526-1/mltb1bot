40eb8539405dc6f94fce0871c4d6b90a582c0413
Xtron [@pro_noober] <84863955+okaybro526-1@users.noreply.github.com>
2022-06-10 01:24:17 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,17 +65,18 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Owner 🧑‍💻", "https://t.me/pro_noober")
+    buttons.buildbutton("MLTB Group", "https://t.me/mirrorleechgrp")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
+Hey 👋
+<b>Status</b>: <u>Authorised or Sudo User</u> ✅
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('Hey there 💤\n\n<b>Status</b>: Not authorised for mirror features ✖️ \n\n📮 You can only use me in MLTB Group', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
